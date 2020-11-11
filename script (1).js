f46ca40e0e1d5a5193da8551c649a61c98fe8e3c
Roman <romansudova@gmail.com>
2020-11-11 21:19:22 +0200

    add while/do

--- script (1).js
+++ script (1).js
@@ -51,3 +51,32 @@ if (personalMovieDB.count < 10) {
 }
 
 console.log(personalMovieDB);
+
+// let i = 0;
+// while(i < 2){
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         --i;   
+//     }
+// }
+
+// do {
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         --i;   
+//     }
+// } while (i < 2)
